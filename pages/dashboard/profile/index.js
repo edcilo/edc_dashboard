0715a80c5ff7f5b4443d7b124ac4a80d8fc2fe4d
edcilo <edcilop@gmail.com>
2022-08-28 19:34:30 -0500

    fix import profile card component

--- pages/dashboard/profile/index.js
+++ pages/dashboard/profile/index.js
@@ -1,6 +1,6 @@
 import ssprops_user from "../../../helpers/ssprops_user";
 import DashboardLayout from "../../../layouts/DashboardLayout";
-import ProfileCard from "../../../components/ProfileCard";
+import ProfileCard from "../../../components/User/ProfileCard";
 
 export default function ProfilePage({ user }) {
   return (
